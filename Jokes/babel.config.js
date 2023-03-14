f508cdd74a637cd30c77907f26c7cb9efc7be608
Dimitri Strauneanu <>
2023-03-14 14:08:55 +0200

    [ Project ] - Add configuration files

--- Jokes/babel.config.js
+++ Jokes/babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: 'react-native-dotenv',
+      },
+    ],
+    ['@babel/plugin-proposal-decorators', {legacy: true}],
+  ],
 };
--- Jokes/index.js
+++ Jokes/index.js
@@ -5,5 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import 'reflect-metadata';
+import {Text} from 'react-native';
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
 
 AppRegistry.registerComponent(appName, () => App);
